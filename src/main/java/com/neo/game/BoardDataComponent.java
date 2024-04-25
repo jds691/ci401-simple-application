1b1cfeb0b68c575d3a524565ce5b82bcc494f580
Neo <52429194+jds691@users.noreply.github.com>
2024-04-25 20:04:02 +0100

    Implement beginnings of the BoardRenderComponent

--- src/main/java/com/neo/game/BoardDataComponent.java
+++ src/main/java/com/neo/game/BoardDataComponent.java
@@ -6,4 +6,33 @@ import com.neo.twig.scene.NodeComponent;
  * Contains the state for the game board at any given time.
  */
 public class BoardDataComponent extends NodeComponent {
+    public static final int BOARD_WIDTH = 10;
+    public static final int BOARD_HEIGHT = 20;
+
+    private int[][] boardState;
+
+    @Override
+    public void start() {
+        super.start();
+
+        boardState = new int[BOARD_HEIGHT][BOARD_WIDTH];
+    }
+
+    private void checkCollisions() {
+
+    }
+
+    public boolean checkLineClear(int y) {
+        for (int i = 0; i <= BOARD_WIDTH; i++) {
+            if (getBoardState(i, y) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int getBoardState(int x, int y) {
+        return boardState[y][x];
+    }
 }
--- src/main/java/com/neo/game/BoardRenderComponent.java
+++ src/main/java/com/neo/game/BoardRenderComponent.java
@@ -1,7 +1,11 @@
 package com.neo.game;
 
+import com.neo.twig.annotations.ForceSerialize;
 import com.neo.twig.graphics.RenderComponent;
+import com.neo.twig.resources.ImageResource;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 
 /**
  * Responsible for rendering the current blocks in the board.
@@ -14,15 +18,41 @@ import javafx.scene.canvas.GraphicsContext;
 public class BoardRenderComponent extends RenderComponent {
     private BoardDataComponent dataComponent;
 
+    public float blockSize = 30;
+    @ForceSerialize
+    private ImageResource boardBackground;
+    private ImagePattern backgroundPattern;
+
     @Override
     public void start() {
         super.start();
+
+        backgroundPattern = new ImagePattern(boardBackground.get(), 0, 0, 30, 30, false);
 
         dataComponent = getNode().getComponent(BoardDataComponent.class);
     }
 
     @Override
     protected void drawToContext(GraphicsContext context) {
+        /*
+        Draw steps:
+        - Draw board background
+        - Draw board contents based on dataComponent (use the Side Order chips sprite)
+        - Draw board border
+        - Draw required effects
+        - Draw transitions?
+        */
 
+        context.save();
+        context.setFill(backgroundPattern);
+        context.fillRect(0, 0, BoardDataComponent.BOARD_WIDTH * blockSize + BoardDataComponent.BOARD_WIDTH, BoardDataComponent.BOARD_HEIGHT * blockSize + BoardDataComponent.BOARD_HEIGHT);
+
+        context.setFill(Color.WHITE);
+        for (int i = 0; i < BoardDataComponent.BOARD_HEIGHT; i++) {
+            for (int j = 0; j < BoardDataComponent.BOARD_WIDTH; j++) {
+                context.fillRect(j * blockSize + j, i * blockSize + i, blockSize, blockSize);
+            }
+        }
+        context.restore();
     }
 }
