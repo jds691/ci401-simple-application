1fd6088f38e2b2e5e04b0412461600f9479ec37f
Neo <52429194+jds691@users.noreply.github.com>
2024-04-27 16:22:15 +0100

    Add block image files

--- src/main/java/com/neo/game/Block.java
+++ src/main/java/com/neo/game/Block.java
@@ -2,10 +2,12 @@ package com.neo.game;
 
 public class Block {
     Color color;
+    int tone;
     boolean isMoving;
 
     public Block() {
         color = Color.None; //For some reason enums default to null
+        tone = 0;
         isMoving = false;
     }
 
