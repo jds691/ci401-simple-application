75f0fa68896568fe97239010de6e9200801dd7f7
Neo <52429194+jds691@users.noreply.github.com>
2024-05-11 05:37:11 +0100

    Implement the most boring title screen known to man I might do fancy rendering magic and pulsate the lighting on the icon cells but we'll see

--- src/main/java/com/neo/game/title/TitleControllerComponent.java
+++ src/main/java/com/neo/game/title/TitleControllerComponent.java
@@ -2,20 +2,26 @@ package com.neo.game.title;
 
 import com.neo.twig.Engine;
 import com.neo.twig.annotations.ForceSerialize;
+import com.neo.twig.resources.ImageResource;
 import com.neo.twig.resources.URLResource;
 import com.neo.twig.ui.FXComponent;
 import javafx.event.ActionEvent;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 public class TitleControllerComponent extends FXComponent {
     @ForceSerialize
     private URLResource gameScene;
+
+    @ForceSerialize
+    private ImageResource gameIcon;
+    @ForceSerialize
+    private double iconSize;
 
     @Override
     public Parent generateFXScene() {
@@ -49,9 +55,25 @@ public class TitleControllerComponent extends FXComponent {
         buttonContainer.getChildren().add(quitButton);
         buttonContainer.setId("button-container");
 
+        VBox titleCenter = new VBox();
+        titleCenter.setId("content-center");
+
+        Label titleLabel = new Label("tetris");
+        titleLabel.getStyleClass().add("title-label");
+
+        ImageView icon = new ImageView();
+        icon.setImage(gameIcon.get());
+        icon.setFitWidth(iconSize);
+        icon.setPreserveRatio(true);
+        icon.setSmooth(true);
+
+        titleCenter.getChildren().addAll(titleLabel, icon);
+
         bottom.setLeft(gameVersion);
         bottom.setRight(buttonContainer);
         root.setBottom(bottom);
+        BorderPane.setAlignment(titleCenter, Pos.CENTER);
+        root.setCenter(titleCenter);
 
         return root;
     }
